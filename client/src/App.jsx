c61ff1fb0b649c15b0bf7479e9e22969eff56378
Archan Pandya <pandyaarchan17504@gmail.com>
2024-01-16 12:25:18 +0530

    Created Login

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import SignUp from "./SignUp";
+import Login from "./Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/register" element={<SignUp />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
